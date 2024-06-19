95fbb35a682e2991e33b1e314a44256f24c1cd66
randy <aychilpa@gmail.com>
2024-06-19 09:48:46 -0600

    update java Homework session5

--- Session 4-5/HomeWork5/src/Student.java
+++ Session 4-5/HomeWork5/src/Student.java
@@ -7,7 +7,6 @@ public class Student {
     private String lastName;
     private String dob; // Date of Birth
 
-    // Constructor
     public Student(String firstName, String middleName, String lastName, String dob) {
         this.studentId = new Random().nextInt(10000); // Random number as Student ID
         this.firstName = firstName;
@@ -16,23 +15,20 @@ public class Student {
         this.dob = dob;
     }
 
-    // Method to concatenate first, middle, and last name
     public String getFullName() {
         return firstName + " " + middleName + " " + lastName;
     }
 
-    // Display method
     public void displayStudentDetails() {
         System.out.println("Student ID: " + studentId);
         System.out.println("Full Name: " + getFullName());
         System.out.println("Date of Birth: " + dob);
     }
 
-    // Main method to create instances and display details
     public static void main(String[] args) {
-        Student student1 = new Student("John", "A.", "Doe", "01/01/2000");
-        Student student2 = new Student("Jane", "B.", "Smith", "02/02/2001");
-        Student student3 = new Student("Jim", "C.", "Brown", "03/03/2002");
+        Student student1 = new Student("Randy", "Chilpa.", "Camacho", "02/12/1995");
+        Student student2 = new Student("Lucero", "Maqueda.", "Valdez", "28/06/1998");
+        Student student3 = new Student("Marcos", "Castro.", "castro", "03/03/2002");
 
         student1.displayStudentDetails();
         student2.displayStudentDetails();
